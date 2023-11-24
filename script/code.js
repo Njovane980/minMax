f7bbc0fdf47472f334fcc1acab4e6a838e5c963f
Njovane980 <vuyisekanjovane8@gmail.com>
2023-11-24 12:35:21 +0200

    MinMaxTask

---
+++ script/code.js
@@ -0,0 +1,9 @@
+let Numbers = [2, 6, 12, 18, 40, 42];
+
+// Find the minimum and maximum using Math.min and Math.max
+let minNumber = Math.min(...Numbers);
+let maxNumber = Math.max(...Numbers);
+
+// Display the results
+console.log("The minimum number is: " + minNumber);
+console.log("The maximum number is: " + maxNumber);
